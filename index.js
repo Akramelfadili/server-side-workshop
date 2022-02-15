ab842e392f8d4398104779b338407a3eeb4cca15
Akram <fadili261998@gmail.com>
2022-02-15 11:48:42 +0100

    feat: add mongoose and mongodb connection script

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("akram")
+import dotenv from 'dotenv'
+import app from './app.js'
+import connectToMongo from './mongo.js'
+
+dotenv.config()
+
+async function startServer() {
+     await connectToMongo()
+
+     app.listen(8080, () => {
+          console.log('Server on http://localhost:8080 🚀')
+     })
+}
+
+startServer()
