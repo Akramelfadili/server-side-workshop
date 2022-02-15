794e6c233d5d9d85977c986c6724d12a724cac4a
Akram <fadili261998@gmail.com>
2022-02-15 18:01:00 +0100

    mutation schemas

--- schema.js
+++ schema.js
@@ -23,6 +23,24 @@ type Query {
      videosForHome: [Video!]!  
      video(id: ID!): Video!
 }
+
+input AddVideoInput {
+  title: String!
+  description: String
+  ownerId: ID!
+  thumbnail: String
+  length: Int
+}
+
+type AddVideoResponse {
+  success: Boolean!
+  message: String!
+  video: Video
+}
+
+type Mutation {
+  addVideo(input: AddVideoInput!): AddVideoResponse!
+}
 `
 
 export default typeDefs
